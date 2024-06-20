f19331692139e28197d8867cd708f9575a99a5b9
Judeeeee <43412616+Judeeeee@users.noreply.github.com>
2024-06-20 11:47:22 +0900

    format_contentメソッドが何をしているかわかりにくかったので変更

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 module BlogsHelper
+  # フォームに入力された文字をエスケープし、改行を反映させるメソッド
+  # フォームに入力されたタグは、文字列としてブラウザに表示される
   def format_content(blog)
-    h(blog.content).gsub(/\r?\n/, '<br>')
+    raw h(blog.content).gsub(/\r?\n/, '<br>') # rubocop:disable Rails/OutputSafety
   end
 end
