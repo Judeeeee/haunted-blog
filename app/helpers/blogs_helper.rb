c020764a031fdb259285d501653bf6c1f6606c5f
Judeeeee <43412616+Judeeeee@users.noreply.github.com>
2024-06-22 10:52:50 +0900

    コードを読めばわかるためコメントを削除

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 module BlogsHelper
-  # フォームに入力された文字をエスケープし、改行を反映させるメソッド
-  # フォームに入力されたタグは、文字列としてブラウザに表示される
   def format_content(blog)
     safe_join(blog.content.split(/\r?\n/), tag.br)
   end
